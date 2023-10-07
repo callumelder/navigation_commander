e9ef35b00ef6182edc74e97a48e51c45dc38ab09
callumelder <cjames.02@icloud.com>
2023-10-07 11:42:56 +1000

    made debugger function (code clean up)

--- navigation_commander/exploration_node.py
+++ navigation_commander/exploration_node.py
@@ -86,6 +86,8 @@ class ExplorationNode(Node):
         x = np.linspace(0, 1, self.width)
         y = np.linspace(0, 1, self.height)
 
+        self.ax = fig.add_subplot(111)
+
         # Update costmap coordinates using the origin information
         # WIDTH = abs(2.0*self.origin[0])
         # HEIGHT = abs(2.0*self.origin[1])
@@ -130,14 +132,7 @@ class ExplorationNode(Node):
             frontier_coord = frontier_coords.pop()
             frontier_coords.clear()
 
-            if (DEBUG_WITH_GRAPH):
-                try:
-                    # Draw the frontier map and grid data; after rotating to have the view match
-                    ax = fig.add_subplot(111)
-                    ax.contour(-self.y_2D, self.x_2D, self.grid_data_2D-200, 10)
-                    ax.contour(-self.y_2D, self.x_2D, self.frontier_map, 10, colors=['red'])
-                except:
-                    self.get_logger().warning('Aborting graphing effort...')
+            self.debug_plot_map(DEBUG_WITH_GRAPH, self.ax)
 
             # skips first waypoint
             if frontier_coord == self.get_initial_position():
@@ -148,15 +143,7 @@ class ExplorationNode(Node):
 
             waypoint = self.convert_to_waypoint(frontier_coord)
 
-            if (DEBUG_WITH_GRAPH):
-                try:
-                    # Draw the waypoint on the map; after rotating to have the view match
-                    ax.plot(-waypoint.pose.position.y, waypoint.pose.position.x, 0, marker = '^', color='black')
-                    plt.show(block=False)
-                    plt.pause(1)
-                    fig.clear()
-                except:
-                    self.get_logger().warning('Aborting graphing effort...')
+            self.debug_plot_waypoint(DEBUG_WITH_GRAPH, self.ax, waypoint)
 
             # move to frontier
             self.send_goal_waypoint(waypoint)
@@ -168,7 +155,37 @@ class ExplorationNode(Node):
             frontier_coords.extend(max_coordinates)
 
         self.get_logger().info('Map complete!')
+        
         return
+    
+    def debug_plot_map(self, debugger, ax):
+        """
+        Plots frontier map
+        Written by Isaac
+        """
+        if (debugger):
+                try:
+                    # Draw the frontier map and grid data; after rotating to have the view match
+                    ax.contour(-self.y_2D, self.x_2D, self.grid_data_2D-200, 10)
+                    ax.contour(-self.y_2D, self.x_2D, self.frontier_map, 10, colors=['red'])
+                except:
+                    self.get_logger().warning('Aborting graphing effort...')
+
+    
+    def debug_plot_waypoint(self, debugger, ax, waypoint):
+        """
+        Plots current waypoint to move to on map
+        Written by Isaac
+        """
+        if (debugger):
+                try:
+                    # Draw the waypoint on the map; after rotating to have the view match
+                    ax.plot(-waypoint.pose.position.y, waypoint.pose.position.x, 0, marker = '^', color='black')
+                    plt.show(block=False)
+                    plt.pause(1)
+                    fig.clear()
+                except:
+                    self.get_logger().warning('Aborting graphing effort...')
     
 
     def get_initial_position(self):
