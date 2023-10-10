18ac989d68e14893b54d8d516ac6a32997a3c462
callumelder <cjames.02@icloud.com>
2023-10-10 21:37:47 +1000

    added rviz config

--- launch/test_world.launch.py
+++ launch/test_world.launch.py
@@ -26,15 +26,20 @@ from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
-    launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
+    pkg_dir = os.path.join(get_package_share_directory('navigation_commander'))
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    # launch_dir = os.path.join(pkg_dir, 'launch')
+    turtlebot2_launch_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
+
+    bringup_dir = get_package_share_directory('nav2_bringup')
+    bringup_launch = os.path.join(bringup_dir, 'launch')
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     x_pose = LaunchConfiguration('x_pose', default='-2.0')
     y_pose = LaunchConfiguration('y_pose', default='-0.5')
 
     world = os.path.join(
-        get_package_share_directory('turtlebot3_gazebo'),
+        get_package_share_directory('navigation_commander'),
         'worlds',
         'test_world.world'
     )
@@ -54,19 +59,40 @@ def generate_launch_description():
 
     robot_state_publisher_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(launch_file_dir, 'robot_state_publisher.launch.py')
+            os.path.join(turtlebot2_launch_dir, 'robot_state_publisher.launch.py')
         ),
         launch_arguments={'use_sim_time': use_sim_time}.items()
     )
 
     spawn_turtlebot_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(launch_file_dir, 'spawn_turtlebot3.launch.py')
+            os.path.join(turtlebot2_launch_dir, 'spawn_turtlebot3.launch.py')
         ),
         launch_arguments={
             'x_pose': x_pose,
             'y_pose': y_pose
         }.items()
+    )
+
+    # rviz2_cmd = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         os.path.join(bringup_dir, 'rviz', 'nav2_default_view.rviz')
+    #     )
+    # )
+
+    # Add SLAM launch action (replace 'slam_launch_file' with your SLAM launch file)
+    slam_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(bringup_launch, 'bringup_launch.py')),
+        # launch_arguments={'namespace': namespace,
+        #                   'use_namespace': use_namespace,
+        #                   'slam': slam,
+        #                   'map': map_yaml_file,
+        #                   'use_sim_time': use_sim_time,
+        #                   'params_file': params_file,
+        #                   'autostart': autostart,
+        #                   'use_composition': use_composition,
+        #                   'use_respawn': use_respawn}.items())
     )
 
     ld = LaunchDescription()
@@ -76,5 +102,7 @@ def generate_launch_description():
     ld.add_action(gzclient_cmd)
     ld.add_action(robot_state_publisher_cmd)
     ld.add_action(spawn_turtlebot_cmd)
+    # ld.add_action(rviz2_cmd)  # Add RViz2
+    ld.add_action(slam_cmd)
 
     return ld
