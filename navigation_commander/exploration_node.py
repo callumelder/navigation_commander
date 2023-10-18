a5c521ec2f5c203d07f64f22a17c90b1a741433f
Jasmine Teruel <jasmineteruel0@gmail.com>
2023-10-18 21:41:18 +1000

    added A star algorithm

--- navigation_commander/exploration_node.py
+++ navigation_commander/exploration_node.py
@@ -128,21 +128,6 @@ class ExplorationNode(Node):
             self.pose_callback,
             pose_qos
         )
-        # docs.ros2.org/foxy/api/nav_msgs/msg/Path.html
-        self.path_sub = self.create_subscription(
-            Path,
-            '/Path',
-            self.path_sub_callback,
-            10
-        )
-        #https://wiki.ros.org/move_base
-        #alternatively from move_base http://docs.ros.org/en/api/nav_msgs/html/srv/GetPlan.html
-        self.path_pub = self.create_publisher(
-            PoseStamped,
-            '/move_base/goal',
-            self.path_pub_publisher,
-            10
-        )
         #subscriber for images
         self.image_sub = self.create_subscription(
             Image,
@@ -160,7 +145,8 @@ class ExplorationNode(Node):
         self.width = msg.info.width             #extracts width in grid points from costmap info data
         self.height = msg.info.height           #extracts height in grid points from costmap info data
         self.resolution = msg.info.resolution   #extracts grid resolution from costmap info data, default 0.05 m
-
+        self.lethal_cost = msg.info.lethal_cost_threshold
+        self.costmap = msg.data
         # Grab the origin from the costmap, in meters
         origin = msg.info.origin
         self.origin[0] = origin.position.x
@@ -218,15 +204,6 @@ class ExplorationNode(Node):
             distance = sqrt(pow(x2-x1,2)+pow(y2-y1,2))
             total_distance += distance
         return total_distance
-    
-    def path_pub_publisher(self,goal):
-        """
-        Publishes a set of goal poses for the robot to take in order to get to the "main" goal pose/the end one
-        Run this publisher to publish a set of poses to get to the goal waypoint, then run the subscriber to calculate
-        how much distance those set of poses will cover
-        input: goal pose <- PoseStamped() variable
-        """
-        self.path = self.path_pub.publish(goal)
 
     def image_callback(self, data):
         """
@@ -319,6 +296,133 @@ class ExplorationNode(Node):
             self.get_logger().info('Waiting for goal to finish...')
             time.sleep(3)
     
+    def astar(self, start_index, goal_index):
+        """
+        pretty sure start_index and goal_index here are like actual costmap coordinates, no transformations or anything 
+        """
+        resolution = self.resolution
+        costmap = self.costmap
+        open_list = []
+        closed_list = set()
+        parents = dict()
+        g_costs = dict()
+        f_costs = dict()
+        g_costs[start_index] = 0
+        f_costs[start_index] = 0
+        start_cost = 0 + self.euclidean_dist_astar(start_index, goal_index)
+        open_list.append([start_index,start_cost])
+        shortest_path = []
+        path_found = False
+        while open_list:
+            open_list.sort(key = lambda x:x[1])
+            current_node = open_list.pop(0)[0]
+            closed_list.add(current_node)
+            if current_node == goal_index:
+                path_found = True
+                break
+            neighbors = self.find_neighbors(current_node,resolution)
+            for neighbor_index, step_cost in neighbors:
+                if neighbor_index in closed_list:
+                    continue
+                
+                g_cost = g_costs[current_node] + step_cost
+                h_cost = self.euclidean_dist_astar(neighbor_index,goal_index)
+                f_cost = g_cost + h_cost
+
+                in_open_list = False
+                for idx, element in enumerate(open_list):
+                    if element[0] == neighbor_index:
+                        in_open_list= True
+                        break
+                
+                if in_open_list:
+                    if f_cost < f_costs[neighbor_index]:
+                        g_costs[neighbor_index] = g_cost
+                        f_costs[neighbor_index] = f_cost
+                        parents[neighbor_index] = current_node
+                        open_list[idx] = [neighbor_index, f_cost]
+                else:
+                    g_costs[neighbor_index] = g_cost
+                    f_costs[neighbor_index] = f_cost
+                    parents[neighbor_index] = current_node
+                    open_list.append([neighbor_index, f_cost])
+        
+        if not path_found:
+            return shortest_path
+        if path_found:
+            node = goal_index
+            shortest_path.append(goal_index)
+            while node!= start_index:
+                shortest_path.append(node)
+                node = parents[node]
+        # reverse list
+        shortest_path = shortest_path[::-1]
+        return shortest_path, None
+    
+    def euclidean_dist_astar(self, index, goal_index):
+        """
+        Again, index and goal_index here are from the costmap itself
+        im gonna convert them again
+        """
+        index_x = index % self.width
+        index_y = int(index/self.width)
+        goal_x = goal_index % self.width
+        goal_y = int(goal_index/self.width)
+
+        distance = (index_x - goal_x)**2 + (index_y - goal_y)**2
+        return sqrt(distance)
+    
+    def find_neighbors(self, index, orthogonal_step_cost):
+        """
+        Index is supposed to be its original costmap value here I'm pretty sure.
+        https://github.com/SakshayMahna/Robotics-Playground/blob/main/turtlebot3_ws/src/global_path_planning/scripts/algorithms/astar.py#L51
+        """
+        lethal_cost = self.lethal_cost
+        costmap = self.costmap
+        width = self.width
+        height = self.height
+        neighbors = []
+        diagonal_step_cost = orthogonal_step_cost*sqrt(2)
+        upper = index - width
+        if upper > 0:
+            if costmap[upper] < lethal_cost:
+                step_cost = orthogonal_step_cost + costmap[upper]/255
+                neighbors.append([upper, step_cost])
+        left = index - 1
+        if left%width > 0:
+            if costmap[left]<lethal_cost:
+                step_cost = orthogonal_step_cost + costmap[left]/255
+                neighbors.append([left, step_cost])
+        upper_left = index - width - 1
+        if upper_left > 0 and upper_left%width > 0:
+            if costmap[upper_left] < lethal_cost:
+                step_cost = diagonal_step_cost + costmap[upper_left]/255
+                neighbors.append([index - width - 1, step_cost])
+        upper_right = index - width + 1
+        if upper_right > 0 and (upper_right) % width != (width - 1):
+            if costmap[upper_right] < lethal_cost:
+                step_cost = diagonal_step_cost + costmap[upper_right]/255
+                neighbors.append([upper_right, step_cost])
+        right = index + 1
+        if right % width != (width + 1):
+            if costmap[right] < lethal_cost:
+                step_cost = orthogonal_step_cost + costmap[right]/255
+                neighbors.append([right, step_cost])
+        lower_left = index + width
+        if lower_left <= height*width and lower_left%width!=0:
+            if costmap[lower_left] < lethal_cost:
+                step_cost = diagonal_step_cost + costmap[lower_left]/255
+                neighbors.append([lower_left, step_cost])
+        lower = index + width
+        if lower<= height*width:
+            if costmap[lower] < lethal_cost:
+                step_cost = orthogonal_step_cost + costmap[lower]/255
+                neighbors.append([lower,step_cost])
+        lower_right = index + width + 1
+        if (lower_right) <= height*width and lower_right%width!= (width -1):
+            step_cost = diagonal_step_cost + costmap[lower_right]/255
+            neighbors.append([lower_right, step_cost])
+        return neighbors
 
     def find_highest_frontier_density(self, frontier_map, kernel=12):
         """
